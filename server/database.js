88d31846263a4b0c6609e1a179f6ab24937647a5
Chima Ezeirunne <iclasschima@gmail.com>
2025-07-24 01:54:36 +0100

    fix: debug

--- server/database.js
+++ server/database.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const DB_PATH = process.env.DATABASE_PATH || path.resolve(__dirname, "data.db");
 
+console.log(DB_PATH, "Database path");
+
 const db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error("Error connecting to database:", err.message);
